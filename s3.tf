aad5fddb6b9e70cdf547b8335d42a25353e85bf8
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2020-06-16 19:14:31 +0200

    website - add parameter to configure secured access to S3 bucket in cloudfront module (optional)

--- s3.tf
+++ s3.tf
@@ -17,12 +17,25 @@ locals {
 EOF
 }
 
+data "aws_iam_policy_document" "s3_policy" {
+  count = var.use_s3_origin_identity ? 1 :  0
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["arn:aws:s3:::${local.bucket_name}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [ aws_cloudfront_origin_access_identity.default[0].iam_arn]
+    }
+  }
+}
+
 resource "aws_s3_bucket" "bucket" {
   bucket = local.bucket_name
   region = var.aws_region
   acl    = "private"
 
-  policy = local.s3_bucket_access_all
+  policy = var.use_s3_origin_identity ? data.aws_iam_policy_document.s3_policy[0].json : local.s3_bucket_access_all
 
   website {
     index_document = "index.html"
